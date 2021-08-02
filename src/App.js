6370151b4617a2b2bde473b7bdbda6a86e89636e
Jorge Teja <jorgeantoniotj@gmail.com>
2021-08-02 13:22:41 -0500

    fix: buttons

--- src/App.js
+++ src/App.js
@@ -52,6 +52,9 @@ function App() {
         <button onClick={facebook}>FACEBOOK</button><br/>
         <button onClick={google}>GOOGLE</button><br/>
         <button onClick={apple}>APPLE ID</button>
+        <button onClick={facebook}>FACEBOOK</button><br/>
+        <button onClick={google}>GOOGLE</button><br/>
+        <button onClick={apple}>APPLE ID</button>
         <p>{data}</p>
       </header>
     </div>
